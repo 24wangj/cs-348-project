21701249bf6d7862038c286392058f18b5615728
Jianing Wang <24wangj@gmail.com>
2026-03-23 13:29:01 -0400

    Working add/delete

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 
+import datetime
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,6 +14,7 @@ class Competition(db.Model):
     name = db.Column(db.String(150), nullable=False)
     city = db.Column(db.String(150), nullable=False)
     state = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.Date)
 
     def __repr__(self):
         return f"<Competition {self.id}: {self.name}>"
@@ -26,8 +29,8 @@ def index():
 def seed():
     db.session.query(Competition).delete()
     if Competition.query.count() == 0:
-        db.session.add(Competition(name='Best in West Lafayette', city='French Lick', state='Indiana'))
-        db.session.add(Competition(name='Holy Airball', city='Columbus', state='Ohio'))
+        db.session.add(Competition(name='Best in West Lafayette', city='French Lick', state='Indiana', date=datetime.date.today()))
+        db.session.add(Competition(name='Holy Airball', city='Columbus', state='Ohio', date=datetime.date.today()))
         db.session.commit()
 
     if request.method == 'POST':
@@ -35,6 +38,35 @@ def seed():
         return jsonify({"total": total})
 
     return 'Seed complete. Visit / to view competitions.'
+
+@app.route('/add_comp', methods=['POST'])
+def add_comp():
+    data = request.get_json() or {}
+    competition_date = datetime.datetime.strptime(data["date"], "%Y-%m-%d").date()
+    competition = Competition(
+        name=data["name"],
+        city=data["city"],
+        state=data["state"],
+        date=competition_date,
+    )
+    db.session.add(competition)
+    db.session.commit()
+    return jsonify({
+        "id": competition.id,
+        "name": competition.name,
+        "city": competition.city,
+        "state": competition.state,
+        "date": competition.date.isoformat() if competition.date else "",
+    }), 201
+
+@app.route('/delete_comp', methods=['DELETE'])
+def delete_comp():
+    data = request.get_json() or {}
+    competition_id = data["id"]
+    db.session.delete(db.session.query(Competition).filter(Competition.id == competition_id).first())
+    db.session.commit()
+
+    return jsonify({}), 201
 
 if __name__ == '__main__':
     with app.app_context():
